cfd3579f6d4228e61c014d046a6c8cf0720a9dbe
9crk <admin@9crk.com>
2017-03-22 15:28:27 +0800

    first commit

---
+++ mnist_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# mail:    admin@9crk.com
+# author:  9crk.from China.ShenZhen
+# time:    2017-03-22
+
+import caffe
+import numpy as np
+import cv2
+import sys
+import Image
+import matplotlib.pyplot as plt
+
+model = 'examples/mnist/lenet.prototxt';
+weights = 'examples/mnist/lenet_iter_10000.caffemodel';
+net = caffe.Net(model,weights,caffe.TEST);
+caffe.set_mode_cpu()
+#img = caffe.io.load_image(sys.argv[1], color=False)
+img = cv2.imread(sys.argv[1],0)
+if img.shape != [28,28]:
+    img2 = cv2.resize(img,(28,28))
+    img = img2.reshape(28,28,-1);
+else:
+    img = img.reshape(28,28,-1);
+#revert the image,and normalize it to 0-1 range
+img = 1.0 - img/255.0
+out = net.forward_all(data=np.asarray([img.transpose(2,0,1)]))
+
+print out['prob'][0]
+print out['prob'][0].argmax()
+
+
